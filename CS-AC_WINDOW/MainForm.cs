d03b7c07a19a62f02e02fba6e9a4d62a4982545d
Kyle Sherman <ksherm337@gmail.com>
2017-03-21 20:26:09 -0400

    Updated files and changed form name

--- CS-AC_WINDOW/MainForm.cs
+++ CS-AC_WINDOW/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CS_AC_WINDOW
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
